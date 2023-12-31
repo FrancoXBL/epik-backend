34a5c68accde0a713356eb4492db8dda461e1bad
FranXBL <francoxavierbonin@hotmail.com>
2023-12-31 14:55:23 -0300

    Ahora si conecta xD

--- scr/app.js
+++ scr/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 
-import authRoutes from "./routes/auth.routes.js";
 import homeRoutes from "./routes/home.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import saleHistoryRoutes from "./routes/saleHistory.routes.js"
@@ -12,7 +11,6 @@ app.use(morgan("dev"));
 
 app.use(express.json())
 
-app.use("/api", authRoutes);
 app.use("/api", homeRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", saleHistoryRoutes);
